23262e219a0e2364e8392c0f819585ff2d7cecff
Kn3ghtfall <kn3ghtfall@gmail.com>
2025-11-22 21:22:30 +0000

    Internet OS

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,10 @@
 FROM kasmweb/chrome:1.17.0
 
 USER root
-WORKDIR /home/kasm-user
-RUN apt-get update && apt-get install -y curl unzip && \
-    curl https://rclone.org/install.sh | bash && \
-    rm -rf /var/lib/apt/lists/*
+RUN apt-get update \
+ && apt-get install -y curl unzip fuse \
+ && curl https://rclone.org/install.sh | bash \
+ && rm -rf /var/lib/apt/lists/*
 
 USER kasm-user
 WORKDIR /home/kasm-user
@@ -18,3 +18,4 @@ RUN chmod +x /entrypoint.sh
 
 EXPOSE 6901
 ENTRYPOINT ["/entrypoint.sh"]
+CMD ["/usr/bin/kasm-launch"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
-set -e
 
-mkdir -p ~/.config/rclone
+mkdir -p /home/kasm-user/.local/share/kasm-default-profile
 
-cat <<EOF > ~/.config/rclone/rclone.conf
-[backblaze]
-type = b2
-account = ${B2_ACCOUNT_ID}
-key = ${B2_APPLICATION_KEY}
-endpoint = ${B2_ENDPOINT}
-EOF
+rclone mount b2:my-bucket/chrome-profile /home/kasm-user/.local/share/kasm-default-profile \
+    --allow-other \
+    --uid $(id -u) \
+    --gid $(id -g) &
 
-while true; do
-    rclone sync /home/kasm-user/backups backblaze:Internet-OS
-    sleep 60
-done &
+sleep 2
 
-exec /start-kasm.sh
+exec /usr/bin/kasm-launch
