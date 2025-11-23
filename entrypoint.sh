8a0aea161a9ef04f32fe60cfcf0eb93734a1dd5b
Kn3ghtfall <kn3ghtfall@gmail.com>
2025-11-23 01:27:22 +0000

    Internet OS

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ Xvfb :0 -screen 0 1920x1080x24 &
 fluxbox &
 x11vnc -display :0 -forever -nopw -shared &
 /usr/bin/novnc --vnc localhost:5900 --listen 6080 &
+
 chromium --no-sandbox --user-data-dir=/home/kasm-user/chromium-profile &
 
 while true; do
