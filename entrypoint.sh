bd1cebd2bb3bf442a707cee1fb56bf4c12d6c374
Kn3ghtfall <kn3ghtfall@gmail.com>
2025-11-23 01:51:54 +0000

    Internet OS

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,12 @@
 
 mkdir -p /home/kasm-user/chromium-profile
 
-rclone sync "b2:Internet-OS/chrome-profile" /home/kasm-user/chromium-profile \
-    --b2-account $B2_ACCOUNT_ID --b2-key $B2_APPLICATION_KEY
+rclone sync b2:Internet-OS/chrome-profile /home/kasm-user/chromium-profile
 
 Xvfb :0 -screen 0 1920x1080x24 &
 fluxbox &
+sleep 2
+export DISPLAY=:0
 x11vnc -display :0 -forever -nopw -shared &
 /usr/bin/novnc --vnc localhost:5900 --listen 6080 &
 
@@ -14,6 +15,5 @@ chromium --no-sandbox --user-data-dir=/home/kasm-user/chromium-profile &
 
 while true; do
     sleep 60
-    rclone sync /home/kasm-user/chromium-profile "b2:Internet-OS/chrome-profile" \
-        --b2-account $B2_ACCOUNT_ID --b2-key $B2_APPLICATION_KEY
+    rclone sync /home/kasm-user/chromium-profile b2:Internet-OS/chrome-profile
 done
